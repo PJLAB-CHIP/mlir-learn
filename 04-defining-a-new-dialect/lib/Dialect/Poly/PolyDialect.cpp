98187ae8006aa0a5213b93d0135f01a118041e76
jamesnulliu <jamesnulliu@gmail.com>
2024-09-05 07:02:58 +0000

    Finish 04 and Some Little Refactor!

--- 04-defining-a-new-dialect/lib/Dialect/Poly/PolyDialect.cpp
+++ 04-defining-a-new-dialect/lib/Dialect/Poly/PolyDialect.cpp
@@ -2,11 +2,14 @@
 #include <mlir/IR/Builders.h>
 
 #include "mlir-tutorial/Dialect/Poly/PolyDialect.hpp"
+#include "mlir-tutorial/Dialect/Poly/PolyOps.hpp"
 #include "mlir-tutorial/Dialect/Poly/PolyTypes.hpp"
 
 #include "mlir-tutorial/Dialect/Poly/PolyDialect.cpp.inc"
 #define GET_TYPEDEF_CLASSES
 #include "mlir-tutorial/Dialect/Poly/PolyTypes.cpp.inc"
+#define GET_OP_CLASSES
+#include "mlir-tutorial/Dialect/Poly/PolyOps.cpp.inc"
 
 namespace mlir::tutorial::poly
 {
@@ -16,6 +19,10 @@ void PolyDialect::initialize()
     addTypes<
 #define GET_TYPEDEF_LIST
 #include "mlir-tutorial/Dialect/Poly/PolyTypes.cpp.inc"
+        >();
+    addOperations<
+#define GET_OP_LIST
+#include "mlir-tutorial/Dialect/Poly/PolyOps.cpp.inc"
         >();
 }
 
--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,7 @@
-
 CC=clang CXX=clang++ \
 cmake -S . -G Ninja -B build \
-    -DCMAKE_PREFIX_PATH="$ENV{MLIR_HOME}/lib/cmake/mlir;$ENV{LLVM_HOME}/lib/cmake/llvm"
+    -DCMAKE_PREFIX_PATH="$ENV{MLIR_HOME}/lib/cmake/mlir;$ENV{LLVM_HOME}/lib/cmake/llvm" \
+    -DCMAKE_CXX_STANDARD=17 \
+    -DCMAKE_BUILD_TYPE=Release
 
 cmake --build build -j $(nproc)
